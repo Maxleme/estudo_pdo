932a1c4cab0af8279890ca320e71ef84ae1e1ff7
Max <maxleme_21@hotmail.com>
2025-06-22 13:25:38 -0300

    Corrigido caminho do sqlite na função createConnection e criado novo arquivo para criar mais de um registro na tabela

---
+++ criar-turma.php
@@ -0,0 +1,27 @@
+<?php 
+
+use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$connection = \Alura\Pdo\Infrastructure\Persistence\ConnectionCreator::createConnection();
+$studantRepository = new PdoStudentRepository($connection);
+
+$connection->beginTransaction();
+
+$firstStudent = new Student(
+    null,
+    'João Primeiro',
+    new DateTimeImmutable('1995-05-12')
+);
+$studantRepository->save($firstStudent);
+
+$secondStudent =    new Student(
+    null,
+    'João Segundo',
+    new DateTimeImmutable('1994-01-11')
+);
+$studantRepository->save($secondStudent);
+
+$connection->commit();
